81a10f274d4a3dabb1157e8c2906d76242c693f9
harsha1 <harsha.s@netapp.com>
2022-01-19 21:19:37 +0530

    Navigate programatically

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,10 @@
-export const Home = () => <div>Home</div>
+import { useNavigate } from "react-router-dom"
+export const Home = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+        <div>Home Page</div>
+        <button onClick={() => navigate('order-summary', {replace: true})}>Place Order</button>
+        </>
+    )
+}
